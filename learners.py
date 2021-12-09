78d22bcb3b43c4084a0cc63b43ee9ed26e908048
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2021-12-09 15:38:39 +0100

    Fix none-callable bug

--- learners.py
+++ learners.py
@@ -236,12 +236,14 @@ def execute_script_on_remote(script):
     db.session.add(new_entry)
     db.session.commit()
     
+    venjix_auth_secret = app.config('VENJIX_AUTH_SECRET')
+    
     # send POST request
     response = requests.post(
         url = app.config['VENJIX_URL'] + "/{0}".format(script), 
         headers = {
             'Content-type': 'application/json',
-            'Authorization': app.config('VENJIX_AUTH_SECRET')
+            'Authorization': venjix_auth_secret
             },
         data = payload
     )
