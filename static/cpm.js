176c8354f825c9f901fb4f4bacb2670e6cd47202
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2021-12-13 12:24:30 +0100

    Add client resume handling

--- static/cpm.js
+++ static/cpm.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    var client_iframe_url;
   
     // menu tooltips
     (function () {
@@ -8,6 +10,28 @@ $(function () {
             new bootstrap.Tooltip(tooltipTriggerEl)
         })
     })()
+
+    function CheckOpenedVncTab(){
+        try {
+            // try opening a window
+            var winref = window.open('', 'viewer-tab', '', true);
+            // if it succeeded: No viewer-tab opened, close it
+            if(winref.location.href === 'about:blank'){
+                winref.close();
+            }
+            return false;
+        } catch (error) {
+            // if CORS-object error
+            return true;
+        }
+    }
+
+    if(CheckOpenedVncTab()) {
+        client_iframe_url = $('#client').attr('src');
+        $("#client").attr('src', '');
+        $('.pager').addClass('hideContent').removeClass('visibleContent');
+        $('#client-resume').removeClass('hideContent')
+    }
 
     function toggleContent(href = null) {
         
@@ -31,12 +55,54 @@ $(function () {
         
         // toggle content 
         $('.pager').addClass('hideContent').removeClass('visibleContent');
-        $(anker).removeClass('hideContent').addClass('visibleContent');
+
+        if ((anker == '#client') && ($("#client").attr('src') == '')) {
+            $('#client-resume').removeClass('hideContent')
+        } else {
+            $(anker).removeClass('hideContent').addClass('visibleContent');
+        }   
+        $(anker).addClass('visibleContent');     
         
-        // set href for new-tab link
-        current_location = $('.visibleContent').attr('src');
-        $('#menu-newtab').attr('href', current_location)
+        $('#menu-newtab').unbind().click(function() {newTab();})
+
     }
+    
+    function newTab() {
+        current_location = $('.visibleContent').attr('src');
+
+        if ($("#client").hasClass("visibleContent") &&
+            $("#client").attr('src') != '') {
+                // save location
+                client_iframe_url = current_location
+                // unload iframe
+                $("#client").attr('src', '')
+                // open noVNC in new tab
+                let w = window.open(current_location, '_blank');
+                w.name = "viewer-tab";
+
+                $('.pager').addClass('hideContent').removeClass('visibleContent');
+                $('#client-resume').removeClass('hideContent')
+        } else {
+            window.open(current_location, '_blank')
+        }
+    }
+
+    $('#resume-btn').click(function() {
+        // get control over tab
+        let w = window.open("", "viewer-tab");
+        // close tab
+        w.close('viewer-tab')
+        
+        if(CheckOpenedVncTab()) {
+            alert("Close tab manually")
+        } else {   
+            // return to iFrame viewer
+            $("#client").attr('src', client_iframe_url)
+            
+            $('.pager').addClass('hideContent').removeClass('visibleContent');
+            $('#client').removeClass('hideContent').addClass('visibleContent');
+        }
+    })
 
     // remove preloader
     $('#preloader').addClass('hideContent');
