35a8982081e28264213c474014b3d45e6662d47b
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-02-04 11:57:07 +0100

    Prettify codebase

--- util/assets.py
+++ util/assets.py
@@ -3,9 +3,9 @@ from flask_assets import Bundle
 def get_bundle(theme):
     theme_bundle = {
         'theme.css': Bundle(
-        'main_{0}.scss'.format(theme),
-        filters='libsass',
-        depends='*.scss',
-        output='gen/theme.%(version)s.css'
+            'main_{0}.scss'.format(theme),
+            filters='libsass',
+            depends='*.scss',
+            output='gen/theme.%(version)s.css'
         )}
     return theme_bundle
