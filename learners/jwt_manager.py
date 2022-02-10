e8e4ee059fb980273ac24283de294f4b614db73e
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-02-10 17:42:36 +0100

    Fix config error

--- learners/jwt_manager.py
+++ learners/jwt_manager.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from learners.config import cfg
 from flask_jwt_extended import JWTManager
+from learners.conf.config import cfg
 
 
 jwt = JWTManager()
@@ -9,19 +9,19 @@ jwt = JWTManager()
 @jwt.expired_token_loader
 def token_expired(jwt_header, jwt_payload):
     error_msg = "Your token is expired. Please login again."
-    return render_template("login.html", cfg.template, error=error_msg)
+    return render_template("login.html", **cfg.template, error=error_msg)
 
 
 @jwt.invalid_token_loader
 def token_invalid(jwt_payload):
     error_msg = "Your token is invalid."
-    return render_template("login.html", cfg.template, error=error_msg)
+    return render_template("login.html", **cfg.template, error=error_msg)
 
 
 @jwt.unauthorized_loader
 def token_missing(callback):
     error_msg = "Authorization is missing."
-    return render_template("login.html", cfg.template, error=error_msg)
+    return render_template("login.html", **cfg.template, error=error_msg)
 
 
 def init_jwt(app):
