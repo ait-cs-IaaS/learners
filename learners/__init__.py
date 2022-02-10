c7d71e8b7bb7b639252441b5ff9291ab6fd50430
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-02-10 17:42:05 +0100

    Cleanup configuration process

--- learners/__init__.py
+++ learners/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-
-from learners.config import build_config
-from learners.database import build_db
-from learners.jwt_manager import init_jwt
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 
 def main():
@@ -10,9 +8,43 @@ def main():
     app = Flask(__name__)
 
     with app.app_context():
+
+        """
+        Set global configuration
+
+        The configuration is set via the YAML file 'learners_config.yml' of the current working directory.
+        To use a different config file, the path can be set via the environmant variable 'LEARNERS_CONFIG'.
+        An example configuration can be found in 'learners_config.yml' in the root directory.
+        """
+
+        from learners.conf.config import build_config
+
         build_config(app)
+
+        """
+        Set up the database
+
+        Learners keeps track of the training/exercise progress of the participants, for this
+        a locale database is created, which is initialized with the following function.
+        """
+
+        from learners.database import build_db
+
         build_db(app)
+
+        """
+        Initialize JWT manager
+
+        Learners uses JWT tokens for authentication, the necessary JWT manager is initialized
+        in the following function.
+        """
+
+        from learners.jwt_manager import init_jwt
+
         init_jwt(app)
+
+        CORS(app)
+        Bcrypt(app)
 
     from learners import views
 
