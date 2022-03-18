6f821e74e62a1f2efb0820d65d2a2766f378ffe6
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-03-18 18:14:12 +0100

    Add admin-required decorator

--- learners/jwt_manager.py
+++ learners/jwt_manager.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import redirect, render_template
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended import verify_jwt_in_request
+from flask_jwt_extended import get_jwt
+
 from learners.conf.config import cfg
+from functools import wraps
 
 
 jwt = JWTManager()
@@ -22,6 +26,24 @@ def token_invalid(jwt_payload):
 def token_missing(callback):
     error_msg = "Authorization is missing."
     return render_template("login.html", **cfg.template, error=error_msg)
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["admin"]:
+                cfg.template["authenticated"] = True
+                return fn(*args, **kwargs)
+            else:
+                error_msg = "Admin only!"
+                cfg.template["authenticated"] = False
+                return render_template("login.html", **cfg.template, error=error_msg)
+
+        return decorator
+
+    return wrapper
 
 
 def init_jwt(app):
