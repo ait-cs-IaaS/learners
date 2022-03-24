f0a70cf773f745f010b1ca64586ad99cec13f97f
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-03-25 00:25:49 +0100

    Add generic exercise model

--- learners/database.py
+++ learners/database.py
@@ -22,6 +22,22 @@ class User(db.Model):
     formExercises = db.relationship("FormExercise", backref="user", lazy=True)
 
 
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    script = db.Column(db.String(120), nullable=True)
+    execution_timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    response_timestamp = db.Column(db.DateTime, nullable=True)
+    response_content = db.Column(db.Text, nullable=True)
+    form_data = db.Column(db.String(), nullable=True)
+    msg = db.Column(db.String(240), nullable=True)
+    call_uuid = db.Column(db.String(120), unique=True, nullable=True)
+    completed = db.Column(db.Integer, nullable=False, default=0)
+    connection_failed = db.Column(db.Integer, nullable=False, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
 # History of sent POSTs
 class ScriptExercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,17 +61,10 @@ class FormExercise(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
 
-class TokenBlocklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(36), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-
-
 @event.listens_for(User.__table__, "after_create")
 def insert_initial_users(*args, **kwargs):
     for user, _ in cfg.users.items():
         db.session.add(User(username=user))
-
     db.session.commit()
 
 
