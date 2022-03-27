e5278d44fc21e07cd7a527980131c6bf1c841ea3
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-03-28 00:11:39 +0200

    Outsource table creation

---
+++ learners/functions/results.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+
+from learners.functions.database import get_all_exercises, get_all_users, get_completed_state
+from learners.logger import logger
+
+
+def construct_results_table(exercises, users) -> Tuple[dict, dict]:
+
+    try:
+        exercises = get_all_exercises()
+        users = get_all_users()
+
+        rows = []
+        for user in users:
+            row = {"user_id": user.id, "username": user.name}
+            for exercise in exercises:
+                if completed_state := get_completed_state(user.id, exercise.id):
+                    row[exercise.name] = int(any(completed_state))
+                else:
+                    row[exercise.name] = -1
+            rows.append(row)
+
+        columns = [{"col_name": "id", "col_id": "user_id"}, {"col_name": "user", "col_id": "username"}]
+        columns.extend({"col_id": exercise.name, "col_name": exercise.pretty_name} for exercise in exercises)
+
+        return {"columns": columns, "rows": rows}
+
+    except Exception as e:
+        logger.exception(e)
+        return None
