7950dac39d41437d1d990744a8b3ef8d2c520d6b
Benjamin Akhras <b@akhras.at>
2022-08-18 16:35:13 +0200

    simplify compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -f ${LEARNERS_CONFIG} ]]; then
   wget --output-document=${LEARNERS_CONFIG} --quiet https://raw.githubusercontent.com/ait-cs-IaaS/learners/${LEARNERS_VERSION}/learners_config.yml
-  sed -i 's#sqlite:///learners_tracker.db#sqlite:////opt/learners/data/db/learners_tracker.db#g' ${LEARNERS_CONFIG}
+  sed -i 's#sqlite:///learners_tracker.db#sqlite:////opt/learners/data/learners_tracker.db#g' ${LEARNERS_CONFIG}
 fi
 
 gunicorn --bind 0.0.0.0:${LEARNERS_PORT:-8080} learners:app
