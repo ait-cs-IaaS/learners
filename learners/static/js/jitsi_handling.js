cdc13f6eb785e8e89438e97f7f3818e77cfdb227
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-09-06 20:23:41 +0200

    Remove unused libs

--- learners/static/js/jitsi_handling.js
+++
@@ -1,7 +0,0 @@
-const domain = "https://localhost:8443"
-const options = {
-    roomName: 'JitsiMeetAPIExample',
-    parentNode: document.querySelector('#presentation')
-};
-
-api = new JitsiMeetExternalAPI(domain, options)
