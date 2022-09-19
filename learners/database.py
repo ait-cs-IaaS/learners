251fbc48270a607489a5bf4139c39e6754e78bdc
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-09-19 21:34:04 +0200

    Add questionaires

--- learners/database.py
+++ learners/database.py
@@ -10,13 +10,14 @@ def build_db(app):
     global db
 
     from learners.conf.db_models import Execution, Exercise, TokenBlocklist, User
-    from learners.functions.database import insert_exercises, insert_initial_users
+    from learners.functions.database import insert_exercises, insert_initial_users, insert_questionaires
 
     db.init_app(app)
     db.create_all()
 
     insert_initial_users()
     insert_exercises(app)
+    insert_questionaires(app)
 
     try:
         loaded_exercises = db.session.query(Exercise).all()
