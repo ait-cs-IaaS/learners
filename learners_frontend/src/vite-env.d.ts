38c6c3c2c445789583494f016961c0288fe08120
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-06 16:41:02 +0100

    Add yml/yaml declarations

--- learners_frontend/src/vite-env.d.ts
+++ learners_frontend/src/vite-env.d.ts
@@ -5,3 +5,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+declare module "*.yaml" {
+  const value: any;
+  export default value;
+}
+declare module "*.yml" {
+  const value: any;
+  export default value;
+}
