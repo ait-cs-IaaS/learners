3dc7b9b63dbd21bc68627b147bef7dc527764b3f
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-06 16:38:53 +0100

    Add route

--- learners_frontend/src/router/index.ts
+++ learners_frontend/src/router/index.ts
@@ -8,12 +8,27 @@ const routes = [
     children: [
       {
         path: "",
-        name: "Home",
+        name: "Mainpage",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+          import(/* webpackChunkName: "home" */ "@/views/Mainpage.vue"),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "Login",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Login.vue"),
       },
     ],
   },
