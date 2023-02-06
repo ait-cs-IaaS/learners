82f646306284de250e826b410ba56f6c9edfbd9a
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-06 16:40:41 +0100

    Get rubik font

--- learners_frontend/src/plugins/webfontloader.ts
+++ learners_frontend/src/plugins/webfontloader.ts
@@ -11,7 +11,7 @@ export async function loadFonts() {
 
   webFontLoader.load({
     google: {
-      families: ["Roboto:100,300,400,500,700,900&display=swap"],
+      families: ["Rubik:100,300,400,500,700,900&display=swap"],
     },
   });
 }
