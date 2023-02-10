e6523473957a5dbe33f6a904772146fb04801b74
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-10 23:11:23 +0100

    Update axios defaults

--- learners_frontend/src/axios.ts
+++ learners_frontend/src/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { store } from "@/store";
 
 axios.defaults.baseURL = "http://localhost:5000/";
-axios.defaults.headers.common["Authorization"] =
-  "Bearer " + localStorage.getItem("token");
+
+let token = store.getters.getJwt;
+if (token) axios.defaults.headers.common["Authorization"] = "Bearer " + token;
