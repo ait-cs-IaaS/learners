849e69d2671afa730ac2730b732a3e48e74fa0f6
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-10 23:21:23 +0100

    Add authentication endpoint

--- learners_backend/routes/authentication.py
+++ learners_backend/routes/authentication.py
@@ -10,41 +10,66 @@ from learners_backend.functions.authentication import check_password
 authentication_api = Blueprint("authentication_api", __name__)
 
 
-@authentication_api.route("/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "GET":
-        try:
-            verify_jwt_in_request()
-            success_msg = f"Logged in as {get_jwt_identity()}."
-            return render_template("login.html", **cfg.template, success=True, success_msg=success_msg)
-        except Exception:
-            return render_template("login.html", **cfg.template)
-
+@authentication_api.route("/login", methods=["POST"])
+def postLoginData():
     data = request.get_json()
 
     username = data.get("username", None)
     password = data.get("password", None)
 
-    print(username)
-    print(password)
-
     if not check_password(cfg.users, username, password):
-        error_msg = "Invalid username or password"
         return jsonify(authenticated=False)
-        return render_template("login.html", **cfg.template, error=error_msg)
 
     admin = cfg.users.get(username).get("admin")
-    cfg.template["admin"] = admin
-    cfg.template["authenticated"] = True
     user_role = cfg.users.get(username).get("role")
 
-    access_token = create_access_token(identity=username, additional_claims={"admin": admin, "role": user_role})
-    # response = make_response(redirect("/result/all", 302)) if admin else make_response(redirect("/access", 302))
-    response = jsonify(authenticated=True, token=access_token)
-    response.set_cookie("token", value=access_token, secure=True, httponly=False)
+    jwt = create_access_token(identity=username, additional_claims={"admin": admin, "role": user_role})
 
-    return response
+    return jsonify(authenticated=True, jwt=jwt)
+
+
+@authentication_api.route("/authentication", methods=["GET"])
+@jwt_required(optional=True)
+def verifyAuthentication():
+    current_identity = get_jwt_identity()
+    return jsonify(user=current_identity)
+
+
+# @authentication_api.route("/login", methods=["GET", "POST"])
+# def login():
+
+#     if request.method == "GET":
+#         try:
+#             verify_jwt_in_request()
+#             success_msg = f"Logged in as {get_jwt_identity()}."
+#             return render_template("login.html", **cfg.template, success=True, success_msg=success_msg)
+#         except Exception:
+#             return render_template("login.html", **cfg.template)
+
+#     data = request.get_json()
+
+#     username = data.get("username", None)
+#     password = data.get("password", None)
+
+#     print(username)
+#     print(password)
+
+#     if not check_password(cfg.users, username, password):
+#         error_msg = "Invalid username or password"
+#         return jsonify(authenticated=False)
+#         return render_template("login.html", **cfg.template, error=error_msg)
+
+#     admin = cfg.users.get(username).get("admin")
+#     cfg.template["admin"] = admin
+#     cfg.template["authenticated"] = True
+#     user_role = cfg.users.get(username).get("role")
+
+#     jwt = create_access_token(identity=username, additional_claims={"admin": admin, "role": user_role})
+#     # response = make_response(redirect("/result/all", 302)) if admin else make_response(redirect("/access", 302))
+#     response = make_response(jsonify(authenticated=True, jwt=jwt))
+#     response.set_cookie("jwt_cookie", value=jwt, secure=True, httponly=False)
+
+#     return response
 
 
 @authentication_api.route("/logout")
