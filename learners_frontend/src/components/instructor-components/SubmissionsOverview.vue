b096caffbb49cdc9374ec0ef15a733fd0e6d80cc
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-23 22:12:56 +0100

    Add submission card component

--- learners_frontend/src/components/instructor-components/SubmissionsOverview.vue
+++ learners_frontend/src/components/instructor-components/SubmissionsOverview.vue
@@ -1,30 +1,70 @@
 <template>
   <div>
+    <v-dialog v-model="showLoader" :scrim="false" persistent width="auto">
+      <loader />
+    </v-dialog>
+
     <h2>Submissions Overview</h2>
-    <v-table>
+    <v-table class="mt-2">
       <thead>
         <tr>
-          <th class="text-left" v-for="col in cols" :key="col.id">
-            {{ col.name }}
+          <th
+            class="text-left"
+            v-for="exercise in exercises"
+            :key="exercise.id"
+          >
+            {{ exercise.name }}
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="row in rows" :key="row.user_id">
-          <td class="text-left" v-for="col in cols" :key="col.id">
-            <success-icon v-if="row[col.id] === 1" />
-            <fail-icon v-else-if="row[col.id] === -1" />
-            <span v-else>{{ row[col.id] }}</span>
+        <tr v-for="submission in submissions" :key="submission.user_id">
+          <td
+            class="text-left"
+            v-for="exercise in exercises"
+            :key="exercise.id"
+          >
+            <!-- Success Icon -->
+            <success-icon
+              class="clickable"
+              v-if="submission[exercise.id].completed === 1"
+              @click="showDetails(exercise.id, submission['user_id'])"
+            />
+
+            <!-- Fail Icon -->
+            <fail-icon
+              class="clickable"
+              v-else-if="
+                submission[exercise.id].completed === -1 &&
+                submission[exercise.id].executions.len > 0
+              "
+            />
+            <!-- Username -->
+            <span v-else-if="exercise.id === 'username'">{{
+              submission[exercise.id]
+            }}</span>
+            <!-- Empty -->
+            <span v-else> - </span>
           </td>
         </tr>
       </tbody>
     </v-table>
+
+    <!-- Dialog -->
+    <v-dialog v-model="dialog" width="60%">
+      <submission-card
+        :userId="detailsIdentifier.userId"
+        :exerciseId="detailsIdentifier.exerciseId"
+      />
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import SuccessIcon from "../sub-components/SuccessIcon.vue";
-import FailIcon from "../sub-components/FailIcon.vue";
+import SubmissionCard from "@/components/sub-components/SubmissionCard.vue";
+import SuccessIcon from "@/components/sub-components/SuccessIcon.vue";
+import FailIcon from "@/components/sub-components/FailIcon.vue";
+import { store } from "@/store";
 import axios from "axios";
 
 export default {
@@ -32,24 +72,40 @@ export default {
   components: {
     SuccessIcon,
     FailIcon,
+    SubmissionCard,
   },
   data() {
     return {
-      tabledata: { cols: <any>[], rows: <any>[] },
-      submissions: [],
-      cols: <any>[{ id: "username", name: "user" }],
-      rows: <any>[],
+      exercises: <any>[{ id: "username", name: "user" }],
+      submissions: <any>[],
+      dialog: false,
+      detailsIdentifier: { userId: 0, exerciseId: "" },
+      loading: false,
     };
   },
-  computed: {},
-  methods: {},
+  props: {
+    currentTab: { type: String, require: false },
+  },
+  computed: {
+    showLoader() {
+      const viewCondition = store.getters.getCurrentView === "admin";
+      const tabCondition = this.currentTab === "Submissions";
+      const eventCondition = this.loading;
+      return viewCondition && tabCondition && eventCondition;
+    },
+  },
+  methods: {
+    showDetails(exerciseId, userId) {
+      this.dialog = true;
+      this.detailsIdentifier = { userId: userId, exerciseId: exerciseId };
+    },
+  },
   async beforeMount() {
     axios.get("exercises").then((res) => {
       const exercises = res.data.exercises;
       exercises.sort((a, b) => (a.order_weight > b.order_weight ? 1 : -1));
       exercises.forEach((exercise) => {
-        console.log(exercise);
-        this.cols.push({
+        this.exercises.push({
           id: exercise.global_exercise_id,
           name: exercise.exercise_name,
         });
@@ -58,9 +114,8 @@ export default {
 
     axios.get("submissions").then((res) => {
       const submissions = res.data.submissions;
-      console.log(submissions);
       submissions.forEach((submission) => {
-        this.rows.push(submission);
+        this.submissions.push(submission);
       });
     });
   },
---
+++ learners_frontend/src/components/sub-components/SubmissionCard.vue
@@ -0,0 +1,181 @@
+<template>
+  <v-card
+    class="pa-5 d-flex justify-center align-content-center"
+    min-height="450"
+  >
+    <loader v-if="loading" width="300px" />
+
+    <!-- Header -->
+    <v-toolbar-title v-if="!loading" class="details-card-title">
+      <span class="text-grey">Exercise:</span>
+      <h1>{{ exerciseName }}</h1>
+      <span class="text-grey">User:</span>
+      <h2>{{ userName }}</h2>
+    </v-toolbar-title>
+
+    <!-- Content -->
+    <v-card-text v-if="!loading" class="details-card-text">
+      <div
+        v-for="(submission, index) in submissions"
+        :key="submission"
+        class="details-card-submission-row"
+        :class="{ 'previous-submission': index > 0 }"
+      >
+        <!-- Submission Header -->
+        <v-container class="details-card-submission-header mb-4">
+          <v-row>
+            <v-col cols="1">
+              <success-icon v-if="submission.completed === 1" />
+              <fail-icon v-else-if="submission.completed === -1" />
+            </v-col>
+            <v-col cols="11">
+              <h2>Submission #{{ submissions.length - index }}</h2>
+              <span class="text-grey">
+                Executed on: {{ submission.execution_timestamp }}
+              </span>
+            </v-col>
+          </v-row>
+        </v-container>
+        <!-- Submission's fields -->
+        <div
+          v-for="(input, label) in extractFormData(submission)"
+          :key="label"
+          class="details-card-row"
+          :class="{
+            missing: String(input).length < 1,
+          }"
+        >
+          <div class="details-card-label">
+            {{ unescape(label) }}
+          </div>
+          <div class="details-card-input">
+            {{ input }}
+          </div>
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import SuccessIcon from "@/components/sub-components/SuccessIcon.vue";
+import FailIcon from "@/components/sub-components/FailIcon.vue";
+import Loader from "@/components/sub-components/Loader.vue";
+import axios from "axios";
+
+export default {
+  name: "SubmissionCard",
+  components: {
+    SuccessIcon,
+    FailIcon,
+    Loader,
+  },
+  props: {
+    userId: { type: Number, require: true },
+    exerciseId: { type: String, require: true },
+  },
+  computed: {},
+  data() {
+    return {
+      exerciseName: "",
+      userName: "",
+      submissions: <any>[],
+      loading: false,
+    };
+  },
+  methods: {
+    unescape(_string) {
+      return _string.replaceAll("_", " ");
+    },
+    extractFormData(submission) {
+      const form_data = JSON.parse(submission.form_data);
+      return Object.values(form_data)[0];
+    },
+  },
+  async beforeMount() {
+    this.loading = true;
+    const url = `submissions/${this.userId}/${this.exerciseId}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        const submissions = res.data.submissions;
+        this.exerciseName = res.data.exercise_name;
+        this.userName = res.data.user_name;
+        submissions.forEach((submission) => {
+          this.submissions.push(submission);
+        });
+      })
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
+
+<style lang="scss">
+.details-card-title {
+  padding: 10px 24px;
+  span {
+    font-size: 0.85rem;
+    display: block;
+    margin-top: 5px;
+  }
+  h1 {
+    font-size: 1.6rem;
+  }
+  h2 {
+    font-size: 1.2rem;
+  }
+}
+
+.details-card-submission-row {
+  margin-bottom: 40px;
+}
+
+.details-card-submission-header {
+  display: flex;
+  h2 {
+    font-size: 1.2rem;
+    color: rgb(var(--v-theme-primary));
+  }
+  .success-checkmark {
+    display: inline-flex;
+    margin-bottom: -6px;
+    margin-left: 0 4px;
+  }
+  span {
+    font-size: 0.85rem;
+    display: block;
+    margin-top: 5px;
+  }
+}
+
+.details-card-text {
+  flex-grow: 1;
+  overflow-y: auto;
+}
+
+.details-card-row {
+  display: block;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 1px 1px 4px #c1c1c1;
+  border-left: 5px solid rgb(var(--v-theme-primary));
+  &.missing {
+    border-left: 5px solid #9e9e9e;
+    background-color: #ececec;
+  }
+}
+
+.details-card-label {
+  text-transform: capitalize;
+  font-size: 0.95rem;
+  padding: 4px 10px;
+  color: #9e9e9e !important;
+}
+.details-card-input {
+  font-size: 0.95rem;
+  display: block;
+  padding: 4px 10px;
+}
+</style>
