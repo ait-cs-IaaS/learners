da20ff8cd5713d2a144958d3b73e5d04503eb057
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-23 22:11:24 +0100

    Add submissions endpoints to execution

--- learners_backend/routes/execution.py
+++ learners_backend/routes/execution.py
@@ -4,22 +4,26 @@ import uuid
 
 from flask import Blueprint, jsonify, request, send_from_directory
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from learners_backend.jwt_manager import jwt_required_any_location
+from learners_backend.jwt_manager import admin_required, jwt_required_any_location, only_self_or_admin
 from learners_backend.logger import logger
 from learners_backend.functions.database import (
     db_create_execution,
     db_create_file,
+    get_all_exercises,
+    get_all_users,
     get_completed_state,
     get_current_executions,
+    get_executions_by_user_exercise,
     get_exercise_by_global_exercise_id,
     db_create_questionaire_execution,
     get_exercise_by_name,
     get_exercise_groups,
     get_exercises_by_group,
+    get_user_by_id,
     get_user_by_name,
 )
 from learners_backend.functions.execution import call_venjix, update_execution_response, wait_for_response
-from learners_backend.functions.helpers import allowed_file, append_key_to_dict, append_or_update_subexercise
+from learners_backend.functions.helpers import allowed_file, append_key_to_dict, append_or_update_subexercise, convert_to_dict
 
 
 from werkzeug.utils import secure_filename
@@ -172,3 +176,40 @@ def submit_questionaire(global_questionaire_id):
             response["msg"] = "User not permitted"
 
     return jsonify(response)
+
+
+# NEW
+
+
+@execution_api.route("/submissions", methods=["GET"])
+@admin_required()
+def get_all_submissions():
+
+    users = get_all_users()
+    exercises = get_all_exercises()
+    submissions = []
+
+    for user in users:
+        executions = {"user_id": user.id, "username": user.name}
+        for exercise in exercises:
+            completed_state = [state[0] for state in get_completed_state(user.id, exercise.id)]
+            execution_ids = [
+                execution.get("execution_uuid") for execution in convert_to_dict(get_executions_by_user_exercise(user.id, exercise.id))
+            ]
+            executions[exercise.global_exercise_id] = {
+                "completed": int(any(completed_state)) if completed_state else -1,
+                "executions": execution_ids,
+            }
+        submissions.append(executions)
+
+    return jsonify(submissions=submissions)
+
+
+@execution_api.route("/submissions/<user_id>/<global_exercise_id>", methods=["GET"])
+@only_self_or_admin()
+def get_user_exercise_submissions(user_id, global_exercise_id):
+
+    exercise = get_exercise_by_global_exercise_id(global_exercise_id)
+    db_submissions = get_executions_by_user_exercise(user_id, exercise.id)
+
+    return jsonify(exercise_name=exercise.exercise_name, user_name=get_user_by_id(user_id).name, submissions=convert_to_dict(db_submissions))
