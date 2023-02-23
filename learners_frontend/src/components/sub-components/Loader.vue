f4f05d0ef8bb7e38846b89663f5fa67529a62d42
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-02-23 22:13:05 +0100

    Add loader component

---
+++ learners_frontend/src/components/sub-components/Loader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :style="style" class="align-self-center">
+    <h5 class="text-center">{{ text }}</h5>
+    <v-progress-linear
+      class="mt-3 mb-3"
+      color="primary"
+      indeterminate
+      rounded
+      height="6"
+    ></v-progress-linear>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Loader",
+  props: {
+    text: {
+      type: String,
+      require: false,
+      default: "Requesting data ...",
+    },
+    width: {
+      type: String,
+      require: false,
+      default: "100%",
+    },
+  },
+  computed: {
+    style() {
+      return "width: " + this.width;
+    },
+  },
+};
+</script>
