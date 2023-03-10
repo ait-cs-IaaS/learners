221f9a9fc9e2dfbb93deada5486d7d7a39f6b7ff
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-03-10 11:10:26 +0100

    Allow cookies in CORS requests

--- backend/conf/config.py
+++ backend/conf/config.py
@@ -102,3 +102,5 @@ def config_app(app):
     app.config["JWT_SECRET_KEY"] = cfg.jwt_secret_key
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = cfg.jwt_access_token_expires
     app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "query_string"]
+
+    app.config["CORS_SUPPORTS_CREDENTIALS"] = True
