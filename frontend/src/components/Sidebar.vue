16f1b3d3dab9d9ac70271094d02a1fb9a874625e
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-03-10 11:31:20 +0100

    Disable new tab on admin view

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -108,6 +108,9 @@ export default {
     },
     openNewTab() {
       const currentView = store.getters.getCurrentView;
+
+      if (currentView === "admin") return false;
+
       const activeTab = this.tabs?.find((tab) => {
         return tab.id === currentView;
       });
