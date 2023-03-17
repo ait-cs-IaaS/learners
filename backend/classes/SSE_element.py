043a95d8fa773f2b3ef7c8b5cc5bb4a9d8ac1eaf
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-03-17 12:51:49 +0100

    Update sse element class

--- backend/classes/SSE_element.py
+++ backend/classes/SSE_element.py
@@ -8,17 +8,19 @@ class SSE_element:
         event: string = "",
         message: string = "",
         recipients: list = [],
-        positions: list = [],
+        positions: list = ["all"],
     ):
         self.event = event
         self.message = message
-        self.recipients = recipients
         self.positions = positions
+        self.recipients = recipients
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}(message={self.message}, recipients={self.recipients}, positions={self.positions})"
+        return f"{type(self).__name__}(event={self.event}, message={self.message}, recipients={self.recipients}, positions={self.positions})"
 
     def toJson(self):
-        if self.event == "newNotification":
-            return json.dumps({"message": self.message, "positions": self.positions})
-        return ""
+        return json.dumps({"message": self.message, "positions": self.positions})
+        # if self.event == "newNotification":
+        # if self.event == "newSubmission":
+        #     return json.dumps({"message": self.message, "positions": ["all"]})
+        # return ""
