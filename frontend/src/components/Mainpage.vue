7f15ad1c75ec1ba56930044910c7ebe8666032e4
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-04-04 10:30:16 +0200

    Add questionaire container and sse event

--- frontend/src/components/Mainpage.vue
+++ frontend/src/components/Mainpage.vue
@@ -7,30 +7,41 @@
       />
     </transition>
 
+    <questionaire
+      v-show="showQuestionaires"
+      :currentQuestionaire="currentQuestionaire"
+    />
+
     <frame-pager v-for="tab in filteredTabs" :key="tab.id" :tab="tab" />
     <admin-area v-if="admin" />
+    <user-area />
   </div>
 </template>
 
 <script lang="ts">
 import FramePager from "@/components/general/FramePager.vue";
 import AdminArea from "@/components/admin/AdminArea.vue";
+import UserArea from "@/components/user/UserArea.vue";
+import Notification from "@/components/sub-components/Notification.vue";
+import Questionaire from "@/components/sub-components/Questionaire.vue";
 import { ITabObject } from "@/types";
 import { jwtDecode } from "jwt-js-decode";
 import { store } from "@/store";
-import Notification from "@/components/sub-components/Notification.vue";
-import { extractNotifications } from "@/helpers";
+import { extractNotifications, extractQuestionaires } from "@/helpers";
 
 export default {
   name: "Mainpage",
   components: {
     FramePager,
     AdminArea,
+    UserArea,
     Notification,
+    Questionaire,
   },
   data() {
     return {
       notificationClosed: false,
+      questionaireClosed: false,
       evtSource: EventSource as any,
     };
   },
@@ -53,6 +64,20 @@ export default {
         store.getters.getNotificationsLength > 0 &&
         this.$route.name != "Login"
       );
+    },
+    showQuestionaires() {
+      return (
+        store.getters.getShowQuestionaires &&
+        store.getters.getQuestionairesLength > 0 &&
+        store.getters.getCurrentView === "presentations"
+      );
+    },
+    currentQuestionaire() {
+      let questionaires = store.getters.getQuestionaires;
+      if (questionaires) {
+        let index = store.getters.getCurrentQuestionaireIndex;
+        return questionaires[index];
+      }
     },
   },
   methods: {
@@ -94,12 +119,27 @@ export default {
         store.dispatch("setAdminForceReload", "feedback");
       });
 
+      this.evtSource.addEventListener("newQuestionaire", (event) => {
+        this.questionaireClosed = false;
+        console.log("NEW QUESTIONAIRE");
+        console.log(event);
+        console.log(event.data);
+        console.log(JSON.parse(event.data));
+        console.log(JSON.parse(event.data)?.question);
+        const newQuestionaire = extractQuestionaires(event.data);
+        newQuestionaire.event = "newQuestionaire";
+        console.log(newQuestionaire);
+        // Store actions
+        store.dispatch("appendToQuestionaires", newQuestionaire);
+        store.dispatch("setCurrentQuestionaireToLast");
+        store.dispatch("setAdminForceReload", "questionaire");
+      });
+
       this.evtSource.onerror = function (error) {
         console.error("Connecttion to SSE source lost.");
       };
     },
     closeSSE() {
-      console.log("close that shit");
       console.log(this.evtSource);
       this.evtSource.close();
       console.log("closed");
@@ -111,6 +151,9 @@ export default {
 
     // Get full list of notifications from server
     store.dispatch("getNotificationsFromServer");
+
+    // Get full list of questionaires from server
+    store.dispatch("getQuestionairesFromServer");
   },
   beforeUnmount() {
     this.closeSSE();
