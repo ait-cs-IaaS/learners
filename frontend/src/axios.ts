ca8b00e56d0b31d40b5e8000243ea34e42dde68d
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-04-10 12:30:50 +0200

    Add todo

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { store } from "@/store";
 
+// TODO: Change localhost
 axios.defaults.baseURL = process.env.VUE_APP_API || "http://localhost:5000/";
 
 let token = store.getters.getJwt;
