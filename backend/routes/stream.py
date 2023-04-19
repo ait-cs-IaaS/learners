7a92b1db41bb5a8413aa06e6582ab2a6f09d720a
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-04-19 21:24:09 +0200

    Allow credentials on stream

--- backend/routes/stream.py
+++ backend/routes/stream.py
@@ -1,7 +1,6 @@
 from backend.classes.SSE import sse
 
 from flask import Blueprint, Response
-from flask_cors import CORS, cross_origin
 from flask_jwt_extended import jwt_required, current_user
 
 from backend.logger import logger
@@ -10,7 +9,7 @@ stream_api = Blueprint("stream_api", __name__)
 
 
 @stream_api.route("/stream")
-# @jwt_required()
+@jwt_required()
 def stream():
     def eventStream(user_id):
 
@@ -22,21 +21,4 @@ def stream():
                 msg = f"event: {notification.event}\ndata:{ notification.toJson() }\n\n"
                 yield msg
 
-    return Response(eventStream(1), mimetype="text/event-stream")
-
-
-# @stream_api.route("/stream")
-# @jwt_required()
-# @cross_origin(supports_credentials=True, origins="https://demo.cyberrange.rocks/")
-# def stream():
-#     def eventStream(user_id):
-
-#         sse_queue = sse.listen()
-
-#         while True:
-#             notification = sse_queue.get()
-#             if user_id in notification.recipients:
-#                 msg = f"event: {notification.event}\ndata:{ notification.toJson() }\n\n"
-#                 yield msg
-
-#     return Response(eventStream(current_user.id), mimetype="text/event-stream")
+    return Response(eventStream(current_user.id), mimetype="text/event-stream")
