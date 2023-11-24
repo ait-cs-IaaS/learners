df7e79cb81a202f2b945be3eb1346f050c0e79be
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2023-11-24 07:58:48 +0100

    simplify sse-helper-function

--- backend/functions/helpers.py
+++ backend/functions/helpers.py
@@ -89,25 +89,36 @@ def replace_attachhment_with_url(formData):
     return formData
 
 
-def sse_create_and_publish(event: str = "newNotification", message: str = "", user=None, page=None, exercise=None) -> bool:
+def sse_create_and_publish(
+    event: str = "notificationEvt", message: str = "", user=None, page=None, exercise=None, recipients=None, positions=None
+) -> bool:
     # Import
     from backend.classes.SSE import SSE_Event, sse
     from backend.functions.database import db_create_notification, db_get_admin_users
 
     # Conditional publishing
-    if event == "newSubmission":
+    if event == "submission":
         message = f"<h4>New submission</h4>User: {user.name}<br>Exercise: {exercise.exercise_name}"
         recipients = [admin_user.id for admin_user in db_get_admin_users()]
 
-    if event == "newComment":
+    if event == "comment":
         message = f"<h4>New Comment</h4>User: {user.name}<br>Page: {page}"
         recipients = [admin_user.id for admin_user in db_get_admin_users()]
 
-    new_event = SSE_Event(
-        event=event,
-        message=message,
-        recipients=recipients,
-    )
+    if event == "content":
+        message = """
+            <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="width: 1.5rem; float: left;margin-right: 10px; margin-top: -2px;">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                </svg>
+            Content Update </h3>
+            """
+        message += f'New content: "{page}"'
+
+    if not positions:
+        positions = ["all"]
+
+    new_event = SSE_Event(event=event, message=message, recipients=recipients, positions=positions)
 
     # Create Database entry
     db_create_notification(new_event)
