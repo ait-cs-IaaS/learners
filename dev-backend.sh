1bf362bb0302415623eabc7250b6f96073cd22cf
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2024-01-26 10:59:43 +0100

    Update dev script

--- dev-backend.sh
+++ dev-backend.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/env bash
+
+cleanup() {
+    echo "Stopping processes..."
+    kill -TERM "$gunicorn_pid" "$yarn_pid"
+    wait
+    echo "Processes stopped."
+    exit
+}
+
+trap cleanup INT
+
+cd frontend
+yarn dev &
+yarn_pid=$!
+cd ..
+
+python -m venv venv
+source venv/bin/activate
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -init)
+        pip install -e .
+  esac
+done
+
 mkdir /tmp/learners
-gunicorn backend:app --worker-class gevent --bind unix:/tmp/learners/learners.sock
+
+gunicorn backend:app --worker-class gevent --bind unix:/tmp/learners/learners.sock &
+gunicorn_pid=$!
+
+
+wait -n
+
+cleanup
